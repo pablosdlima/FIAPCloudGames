a973e7c6b28327312d0de0a70feb0a35418598b1
Marcio Torquato <marcio.limat@gmail.com>
2025-12-10 21:51:27 -0300

    adição de configuração de logs estruturados e tambem a inserção na base mongoDB bem como o arquivo docker-compose.yml que contem as informações para subir o mongo e o mongo express

---
+++ src/FIAPCloudGames.Api/Extensions/SerilogExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace FIAPCloudGames.Api.Extensions
+{
+    public static class SerilogExtensions
+    {
+        public static void AddSerilogConfiguration(this WebApplicationBuilder builder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithThreadId()
+                .Enrich.WithProcessId()
+                .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+                .Enrich.WithProperty("ApplicationName", builder.Configuration["Serilog:Properties:Application"])
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+        }
+
+        public static void UseSerilogRequestLoggingConfiguration(this WebApplication app)
+        {
+            app.UseSerilogRequestLogging(options =>
+            {
+                options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} respondeu {StatusCode} in {Elapsed:0.0000} ms";
+                options.GetLevel = (httpContext, elapsed, ex) => ex != null
+                    ? LogEventLevel.Error
+                    : httpContext.Response.StatusCode > 499
+                        ? LogEventLevel.Error
+                        : LogEventLevel.Information;
+                options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+                {
+                    diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+                    diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+                    diagnosticContext.Set("UserAgent", httpContext.Request.Headers["User-Agent"].ToString());
+                    diagnosticContext.Set("RemoteIP", httpContext.Connection.RemoteIpAddress?.ToString());
+                    diagnosticContext.Set("CorrelationId", httpContext.TraceIdentifier);
+                };
+            });
+        }
+    }
+}
--- src/FIAPCloudGames.Api/Program.cs
+++ src/FIAPCloudGames.Api/Program.cs
@@ -1,5 +1,6 @@
 using FIAPCloudGames.Api.Extensions;
 using FIAPCloudGames.IoC;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddJwtAuthenticationConfig(builder.Configuration);
@@ -7,17 +8,22 @@ builder.Services.AddSwaggerDocumentation();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.ConfigureAppDependencies(builder.Configuration);
+builder.AddSerilogConfiguration();
+
+// Substituir o logger padrão pelo Serilog
+builder.Host.UseSerilog();
 
 var app = builder.Build();
+
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwaggerDocumentation();
 }
 
+app.UseSerilogRequestLoggingConfiguration();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
